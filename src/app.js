abc0054bd2e9ded19e794e53238265d80b6b883b
ssarapu_HA <ssarapu@hostanalytics.com>
2018-06-25 18:46:56 +0530

    #3 config changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components';
+import Knex from './components';
 import 'normalize.css/normalize.css';
 
 
 
-ReactDOM.render(<Form/>, document.getElementById('app'));
+ReactDOM.render(<Knex/>, document.getElementById('app'));
 
 
 
